a1147b61e9704d8b90f149bc9cc183a723c07a75
Adam Jahr <adamjahr@gmail.com>
2018-05-15 17:42:18 -0400

    Lesson 6 - Step 1: Creating an event list

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,22 +1,17 @@
 <template>
   <div>
-    <p>{{ title }}</p>
-    <p>{{ date }}</p>
-    <p>{{ time }}</p>
-    <p>{{ attendeeCount }}</p>
+    <p>{{ events.title }}</p>
+    <p>{{ events.date }}</p>
+    <p>{{ events.time }}</p>
+    <p>{{ events.attendeeCount }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'EventCard',
-  data() {
-    return {
-      title: 'Park Cleanup',
-      date: 'Tue May 15 2018 16:26:21 GMT-0400',
-      time: '6:00',
-      attendeeCount: 0
-    }
+  props: {
+    events: Object
   }
 }
 </script>
--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Events Page</h1>
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :events="{title: event.title, date: event.date, time: event.time, attendeeCount: event.attendeeCount}"/>
   </div>
 
 </template>
@@ -13,6 +13,56 @@ export default {
   name: 'Events',
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 1,
+          title: 'Beach Cleanup',
+          date: '28 October 2018',
+          time: '10:00',
+          timestamp: 1540749900,
+          location: '132 N Magnolia Street, Orlando, Florida',
+          description: "Let's cleanup this beach.",
+          organizer: 'Ádam Jahr',
+          category: {
+            sustainability: true,
+            nature: true
+          },
+          attendeeCount: 10
+        },
+        {
+          id: 2,
+          title: 'Park Cleanup',
+          date: '12 Novemer 2018',
+          time: '12:00',
+          timestamp: 1540748600,
+          location: '132 N Magnolia Street, Orlando, Florida',
+          description: "We're going to clean up this park.",
+          organizer: 'Ádam Jahr',
+          category: {
+            sustainability: true,
+            nature: true
+          },
+          attendeeCount: 20
+        },
+        {
+          id: 3,
+          title: 'Homeless Buffet',
+          date: '2 December 2018',
+          time: '2:00',
+          timestamp: 1540729600,
+          location: '132 N Magnolia Street, Orlando, Florida',
+          description: "We're going to cook great food.",
+          organizer: 'Ádam Jahr',
+          category: {
+            homelessness: true
+          },
+          attendeeCount: 30
+        }
+      ]
+    }
   }
 }
 </script>
