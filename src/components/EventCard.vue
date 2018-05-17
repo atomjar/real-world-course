96e9c93106a90dba06c1d77db515091cb834a95b
Adam Jahr <adamjahr@gmail.com>
2018-05-17 16:36:22 -0400

    Passing in events API response via props

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,26 +1,19 @@
 <template>
   <div class="event-card -shadow">
-    <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
-    <h4 class="title">{{ event.title }}</h4>
+    <span class="eyebrow">@{{ events.time }} on {{ events.date }}</span>
+    <h4 class="title">{{ events.title }}</h4>
     <span class="meta">
       <svg class="icon" width="24" height="24">
         <use xlink:href="feather-sprite.svg#users"/></svg>
-      {{ event.attendeeCount }} people going</span>
+      {{ events.attendeeCount }} people going</span>
   </div>
 </template>
 
 <script>
 export default {
   name: 'EventCard',
-  data() {
-    return {
-      event: {
-        title: 'Park Cleanup',
-        date: 'Tue May 15 2018',
-        time: '6:00',
-        attendeeCount: 10
-      }
-    }
+  props: {
+    events: Object
   }
 }
 </script>
