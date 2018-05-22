38e0938c3fe0149be6e990c6174261b6b6e8db31
Dustin Delatore <dustin@delatore.me>
2018-05-22 20:03:28 +0800

    Style snackbar

--- src/components/Snackbar.vue
+++ src/components/Snackbar.vue
@@ -1,20 +1,41 @@
 <template>
-  <div class="snackbar">
-    <slot name="header"></slot>
-    <slot name="paragraph"></slot>
+  <div class="snackbar -text-primary">
+    <icon v-if="hasIcon" :name="iconName" width="60" height="60"></icon>
+    <div>
+      <slot name="header"></slot>
+      <slot name="paragraph"></slot>
+    </div>
   </div>
 </template>
 
 <script>
+import Icon from '@/components/Icon'
 export default {
-  name: 'Snackbar'
+  name: 'Snackbar',
+  components: {
+    Icon
+  },
+  props: {
+    hasIcon: {
+      type: Boolean,
+      default: false
+    },
+    iconName: String
+  }
 }
 </script>
 
 <style scoped>
 .snackbar {
-  border-width: 1px;
-  border-style: solid;
-  background-color: aquamarine;
+  display: flex;
+  align-items: center;
+  margin: 0 0 1em;
+}
+.snackbar h4,
+.snackbar p {
+  margin: 0;
+}
+.snackbar > .icon {
+  margin-right: 1em;
 }
 </style>
