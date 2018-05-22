6f578d02e785f081b15321514b6401691c3c5454
Dustin Delatore <dustin@delatore.me>
2018-05-22 18:28:47 +0800

    Update icon

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -4,17 +4,20 @@
    <h4  class="title">{{ events.title }}</h4>
 
     <span class="meta">
-      <svg class="icon" width="24" height="24">
-        <use xlink:href="feather-sprite.svg#users"/></svg>
+      <icon name="users"></icon>
       {{ events.attendeeCount }} people going</span>
   </div>
 </template>
 
 <script>
+import Icon from '@/components/Icon'
 export default {
   name: 'EventCard',
   props: {
     events: Object
+  },
+  components: {
+    Icon
   }
 }
 </script>
