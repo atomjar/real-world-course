73717081241ff8535b571e1a9bdb93dc63b86ef2
Dustin Delatore <dustin@delatore.me>
2018-05-23 18:19:23 +0800

    Add Icon, Media, and Meta components

---
+++ src/components/MetaField.vue
@@ -0,0 +1,22 @@
+<template>
+  <span class="meta">
+    <icon v-if="iconName" :name="iconName"></icon>
+    <slot></slot>
+  </span>
+</template>
+
+<script>
+import Icon from '@/components/Icon'
+export default {
+  name: 'MetaField',
+  components: {
+    Icon
+  },
+  props: {
+    iconName: String
+  }
+}
+</script>
+
+<style>
+</style>
