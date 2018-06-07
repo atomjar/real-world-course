f8f2218b5d56b361a12550cd91ccb12ad536c5f7
Dustin Delatore <dustin@delatore.me>
2018-06-07 12:38:32 +0100

    Update for prettierrc and upstream components

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>
-    <p>{{ events.title }}</p>
-    <p>{{ events.date }}</p>
-    <p>{{ events.time }}</p>
-    <p>{{ events.attendeeCount }}</p>
+  <div class="event-card -shadow">
+    <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+    <h4 class="title">{{ event.title }}</h4>
+    <span class="meta">
+      <Icon name="users" />
+      {{ event.attendeeCount }} people going</span>
   </div>
 </template>
 
@@ -16,5 +17,11 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.event-card {
+  padding: 20px;
+}
+.event-card > .title {
+  margin: 0;
+}
 </style>
