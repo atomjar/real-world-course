f2810ea4f69a96c454c3a4dff08c2c6351240237
Dustin Delatore <dustin@delatore.me>
2018-06-07 13:11:31 +0100

    Add Icon and NavBar components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Events</router-link> |
-      <router-link to="/create">Create</router-link>
-      <router-link to="/attend">Attend</router-link>
-    </div>
+    <NavBar />
     <router-view/>
   </div>
 </template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
 
 <style>
 html {
