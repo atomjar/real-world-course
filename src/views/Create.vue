6cee1a9b0b38cd034dfbceeba69c0adff5352d4f
Adam Jahr <adamjahr@gmail.com>
2018-06-07 15:21:01 -0400

    Commented out stuff that will be deleted from previous lesson

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -11,30 +11,45 @@
       </select>
 
       <h3>Name & describe your event</h3>
-      <input v-model="event.title" type="text" placeholder="Add an event title"/>
-      <input v-model="event.description" type="text" placeholder="Add a description"/>
+      <div class="field">
+        <label>Title</label>
+        <input v-model="event.title" type="text" placeholder="Add an event title"/>
+      </div>
+
+      <div class="field">
+        <label>Description</label>
+        <input v-model="event.description" type="text" placeholder="Add a description"/>
+      </div>
 
       <h3>Where is your event?</h3>
-      <input v-model="event.location" type="text" placeholder="Add a location"/>
+      <div class="field">
+        <label>Location</label>
+        <input v-model="event.location" type="text" placeholder="Add a location"/>
+      </div>
 
       <h3>When is your event?</h3>
 
-      <datepicker v-model="event.date" placeholder="Select a date"/>
+      <div class="field">
+        <label>Date</label>
+        <datepicker v-model="event.date" placeholder="Select a date"/>
+      </div>
 
-      <label>Select a time</label>
-      <select v-model="event.time">
-        <option v-for="time in times" :key="time">{{ time }}</option>
-      </select>
+      <div class="field">
+        <label>Select a time</label>
+        <select v-model="event.time">
+          <option v-for="time in times" :key="time">{{ time }}</option>
+        </select>
+      </div>
 
-      <input type="submit" value="Submit"/>
+      <input type="submit" class="button -fill-gradient" value="Submit"/>
     </form>
 
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 import Datepicker from 'vuejs-datepicker'
+// import { mapGetters } from 'vuex'
 
 export default {
   name: 'Create',
@@ -42,50 +57,51 @@ export default {
     Datepicker
   },
   data() {
+    var times = []
+    for (var i = 1; i <= 24; i++) {
+      times.push(i + ':00')
+    }
     return {
-      event: {
+      event: this.createNewEvent(),
+      categories: this.$store.state.categories,
+      times
+    }
+  },
+  // computed: {
+  // username() {
+  //   return this.$store.state.user
+  // },
+  //   ...mapGetters({
+  //     categoryLength: 'getCategoriesLength',
+  //     searchCategories: 'getCategoryByString'
+  //   })
+  // },
+  methods: {
+    addEvent() {
+      this.$store.commit('ADD_EVENT', this.event)
+      this.event = this.createNewEvent()
+    },
+    createNewEvent() {
+      const user = this.$store.state.user
+      return {
         category: '',
-        organizer: {},
+        organizer: user,
         title: '',
         description: '',
         location: '',
         date: '',
-        time: ''
-      },
-      categories: [],
-      times: []
-    }
-  },
-  computed: {
-    // username() {
-    //   return this.$store.state.user
-    // },
-    ...mapGetters({
-      categoryLength: 'getCategoriesLength',
-      searchCategories: 'getCategoryByString'
-    })
-  },
-  created() {
-    var times = []
-    for (var i = 1; i <= 24; i++) {
-      times.push(i)
-    }
-    times.map(time => {
-      time += ':00'
-      this.times.push(time)
-    })
-  },
-  mounted() {
-    this.event.organizer = this.$store.state.user
-    this.categories = this.$store.state.categories
-  },
-  methods: {
-    addEvent() {
-      this.$store.commit('ADD_EVENT', this.event)
+        time: '',
+        attendees: {
+          [user.id]: user.name
+        }
+      }
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
+.field {
+  margin-bottom: 24px;
+}
 </style>
