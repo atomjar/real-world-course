7a484332657a8e98fb5c55674ceb39bccbc52ac5
Dustin Delatore <dustin@delatore.me>
2018-06-07 13:11:47 +0100

    Update prettierrc issues

--- src/store.js
+++ src/store.js
@@ -1,12 +1,20 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     user: { firstName: 'Adam', lastName: 'Jahr' },
-    categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community'],
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
     events: []
   },
   getters: {
@@ -29,4 +37,4 @@ export default new Vuex.Store({
     }
   },
   actions: {}
-});
+})
