1bec9ffd4e05b296ab1c12c57a6afb282045b1e8
Dustin Delatore <dustin@delatore.me>
2018-06-07 15:21:10 +0100

    Add Icon

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
+import Icon from '@/components/Icon.vue'
+// registered globally
+Vue.component('Icon', Icon)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
