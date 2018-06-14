57c1ca3b8f16eafce72fdbc87a30b48379180aeb
Adam Jahr <adamjahr@gmail.com>
2018-06-14 00:37:38 -0400

    Changed attendees to an array

--- src/components/form/Form.vue
+++ src/components/form/Form.vue
@@ -99,9 +99,7 @@ export default {
         location: '',
         date: '',
         time: '',
-        attendees: {
-          [user.id]: user.name
-        }
+        attendees: []
       }
     }
   }
