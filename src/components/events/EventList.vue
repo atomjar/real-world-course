adae2fbc0c7f2107557517cfe8b43289e24429b0
Dustin Delatore <dustin@delatore.me>
2018-06-20 11:40:29 +0100

    Add transition group to Event List
    Included is a VelocityJS library for lightweight js animation.

--- src/components/events/EventList.vue
+++ src/components/events/EventList.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="event-list">
-    <EventCard
-      v-for="event in events"
-      v-bind="event"
-      :key="event.title"
-    />
+    <transition-group
+      appear
+      name="staggered-events"
+      v-on:before-enter="beforeEnter"
+      v-on:enter="enter"
+      v-on:leave="leave"
+      :css="false">
+      <EventCard
+        v-for="(event, index) in events"
+        v-bind="event"
+        v-bind:data-index="index"
+        :key="event.title" />
+    </transition-group>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/events/EventCard.vue'
+import Velocity from 'velocity-animate'
 
 export default {
   name: 'EventList',
@@ -18,6 +27,32 @@ export default {
   },
   props: {
     events: Array
+  },
+  methods: {
+    beforeEnter: function (el) {
+      // Hide and offset to the left
+      el.style.opacity = 0
+      el.style.transform = 'translateX(-50px)'
+    },
+    enter: function (el, done) {
+      // Stagger entrance (especially for onload)
+      var delay = el.dataset.index * 150
+      setTimeout(function () {
+        Velocity(
+          el,
+          { opacity: 1, translateX: '0' },
+          { complete: done }
+        )
+      }, delay)
+    },
+    leave: function (el, done) {
+      // Leave to the right (not staggered, looks buggy)
+      Velocity(
+        el,
+        { opacity: 0, translateX: '50px' },
+        { complete: done }
+      )
+    }
   }
 }
 </script>
