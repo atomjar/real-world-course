8545d069c569ed731c0079b41839df8dca6cf690
Dustin Delatore <dustin@delatore.me>
2018-06-22 16:00:53 +0100

    Add transitions for the Attend prompt box

--- src/views/Attend.vue
+++ src/views/Attend.vue
@@ -1,59 +1,55 @@
 <template>
   <div>
-    <transition-group name='prompt'>
-      <div v-if='showPrompt' class="prompt-box -shadow">
-        <h3 class="title">Are you going?
-          <MetaField iconName="users">{{ event.attendees.length }} attending</MetaField></h3>
+    <div v-if="!event">
+      <p>Loading...</p>
+    </div>
+    <div v-else>
+      <transition name='fade-close' v-if="this.event.organizer.name !== this.$store.state.user.name">
+        <div v-if='showPrompt'
+            class="prompt-box -shadow">
+          <h3 class="title">Are you going?
+            <MetaField iconName="users">{{ event.attendees.length }} attending</MetaField></h3>
 
-        <transition-group name="attendButtons" @after-leave="hidePrompt">
-          <Button :onClick="addAttendee" class="-fill-gradient">
-            <span v-if="attend">Yes</span>
-            <Icon v-else name="check-circle"/>
-          </Button>
-          <Button :onClick="notAttending" class="-fill-gray">
-            <span v-if="notattend">No</span>
-            <Icon v-else name="x-circle" />
-          </Button>
-        </transition-group>
+          <Button @click="addAttendee" class="-fill-gradient">Yes</Button>
+          <Button @click="notAttending" class="-fill-gray">No</Button>
+
+          <transition name="cover" @after-enter="hidePrompt">
+            <span v-if='attend' key='attend' class="banner -confirm"></span>
+            <span v-if='notattend' key='notattend' class="banner -deny"></span>
+          </transition>
+        </div>
+      </transition>
+
+      <div class="event-header">
+        <span class="eyebrow">@{{ event.time }} on {{ parsedDate }}</span>
+        <h1 class="title">{{ event.title }}</h1>
+        <media-block>
+          <h5 slot="header">Organized by {{ event.organizer.name }}</h5>
+          <MetaField slot="paragraph" iconName="tag">Category: {{ event.category }}</MetaField>
+        </media-block>
       </div>
 
-      <div>
-        <div class="event-header">
-          <span class="eyebrow">@{{ event.time }} on {{ parsedDate }}</span>
-          <h1 class="title">{{ event.title }}</h1>
-          <media-block>
-            <h5 slot="header">Organized by {{ event.organizer.name }}</h5>
-            <meta-field slot="paragraph" iconName="tag">Category: {{ event.category }}</meta-field>
-          </media-block>
-        </div>
-      <Snackbar v-else barState="!attend ? 'success' : 'info'">
-        <h5 slot="header">{{ !attend ? 'Attending' : 'Not Attending' }}</h5>
-      </Snackbar>
-    </transition-group>
+      <h3 class="location">Location <Icon name="map" /></h3>
+      <address>{{ event.location }}</address>
 
-        <h3 class="location">Location <icon name="map"></icon></h3>
-        <address>{{ event.location }}</address>
+      <h2>Event details</h2>
+      <p>{{ event.description }}</p>
 
-        <h2>Event details</h2>
-        <p>{{ event.description }}</p>
-
-        <h2>Attendees
-          <span class="badge -fill-gradient">{{ event.attendees.length }}</span>
-        </h2>
-        <ul class="list-group">
-          <li v-for="attendee in event.attendees" class="list-item">
-              <h5 slot="header">{{ attendee.name }}</h5>
-          </li>
-        </ul>
+      <h2>Attendees
+        <span class="badge -fill-gradient">{{ event.attendees.length }}</span>
+      </h2>
+      <ul class="list-group">
+        <li v-for="(attendee, index) in event.attendees" :key="index" class="list-item">
+          <b>{{ attendee.name }}</b>
+        </li>
+      </ul>
       </div>
     </div>
-  </div>
 </template>
 
 <script>
 import MetaField from '@/components/MetaField'
 import MediaBlock from '@/components/MediaBlock'
-import Icon from '@/components/Icon'
 import fb from '@/firebaseConfig.js'
 
 const MONTHS = [
@@ -75,13 +71,15 @@ export default {
   name: 'Attend',
   components: {
     MetaField,
-    MediaBlock,
-    Snackbar
+    MediaBlock
   },
   data() {
     return {
       event: null,
-      docRef: null
+      docRef: null,
+      attend: false,
+      notattend: false,
+      showPrompt: true
     }
   },
   created() {
@@ -118,13 +116,16 @@ export default {
       this.docRef.set({ attendees: newAttendees }, { merge: true })
 
       this.event.attendees = newAttendees
+
+      this.attend = true
     },
     notAttending() {
-      this.notattend = false
-      console.log('Not attending')
+      this.notattend = true
     },
     hidePrompt() {
-      this.showPrompt2 = false
+      setTimeout(() => {
+        this.showPrompt = false
+      }, 400)
     }
   }
 }
@@ -132,8 +133,11 @@ export default {
 
 <style scoped>
 .prompt-box {
+  position: relative;
+  overflow: hidden;
   padding: 1em;
   margin-bottom: 24px;
+  transform: scaleY(1);
 }
 .prompt-box > .title {
   margin: 0 0 0.5em;
@@ -147,26 +151,40 @@ export default {
 .prompt-box > button:last-of-type {
   margin-right: 0;
 }
-.prompt-leave-active .prompt-box {
-  transition: height 1s ease-out;
+.prompt-box > .banner {
+  position: absolute;
+  display: flex;
+  width: 250%;
+  height: 500px;
+  overflow: hidden;
+  border-radius: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
 }
-.prompt-leave-to .prompt-box {
+.prompt-box > .banner.-confirm {
+  background: #39b982;
+}
+.prompt-box > .banner.-deny {
+  background: tomato;
+}
+/* Cover Transition CSS */
+.cover-enter-active.banner {
+  transition: width 0.4s ease-in-out, height 0.4s ease-in-out;
+}
+.cover-enter.banner {
+  width: 0;
   height: 0;
 }
-.prompt-leave-active .snackbar {
-  transition: opacity 1s ease-out;
+.cover-enter-to.banner {
+  width: 250%;
+  height: 500px;
 }
-.prompt-leave-to .snackbar {
-  opacity: 0;
-}
-.attendButtons-leave-active .button .icon,
-.attendButtons-leave-active .button span {
+/* Prompt Box Close Transition CSS */
+.fade-close-leave-active {
   transition: all 1s;
 }
-.attendButtons-leave-to .button .icon {
-  opacity: 1;
-}
-.attendButtons-leave-to .button span {
+.fade-close-leave-to {
+  transform: scaleY(0);
   opacity: 0;
 }
 .location {
